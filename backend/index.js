4a9642fa5358f77adacbd68f519ee9b69c28b2c0
MohdShayan <sehbaqureshi@gmail.com>
2025-02-19 18:54:22 +0530

    new file:   backend/controller/job.js 	modified:   backend/index.js 	new file:   backend/model/job.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/routes/job.js 	modified:   package-lock.json

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express"
 import {connectDB} from './connectDb.js';
+import jobRoutes from './routes/job.js';
 import dotenv from 'dotenv';
+import bodyParser from "body-parser";
 dotenv.config();
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api',jobRoutes);
 
 app.get('/', (req, res) => {  
     return res.send('Hello world received a request.');
@@ -13,6 +20,7 @@ app.get('/', (req, res) => {
  app.post('/resume', (req, res) => {  
     return res.send('Received a POST HTTP method');
  });
+
 
  app.listen(PORT, () => {  
     connectDB();
