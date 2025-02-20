ebf34ba740eca0b8c5e35d3df2126761cbcc4824
gunjanghate <ghategunjan@gmail.com>
2025-02-20 08:39:45 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const PORT = 3000;
 const app = express();
 app.use(cors({
-   origin: 'http://localhost:3001',
+   origin: '*',
    methods: ['POST', 'GET', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type'],
 }));
